b821f6a83abb96f32c4d2beb140429040050e92a
forellko <dragonforce97q@gmail.com>
2024-01-29 02:07:31 +0300

    data-types

---
+++ data-types/script.py
@@ -0,0 +1,27 @@
+# str
+a = 'Hello world'
+
+# int
+b = 3
+
+# float
+c = 3.14
+
+# list
+d = [1,2,3]
+
+# range
+# Array of 0-n numbers
+e = range(6)
+
+# dict
+f = {"name": "Zoe", "age": "10"}
+
+# set
+# Unique values
+g = {1,2,3}
+
+# bool
+h = True
+# NoneType
+i = None
